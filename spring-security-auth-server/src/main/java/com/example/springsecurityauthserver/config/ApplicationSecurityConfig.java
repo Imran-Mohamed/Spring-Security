0469692e4e7359241d60c20e4840ae7001483158
Imran Mohamed <imran.mohamed@siemens.com>
2020-10-02 19:52:42 +0530

    pom file change and new config file

---
+++ spring-security-auth-server/src/main/java/com/example/springsecurityauthserver/config/ApplicationSecurityConfig.java
@@ -0,0 +1,6 @@
+package com.example.springsecurityauthserver.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+}
